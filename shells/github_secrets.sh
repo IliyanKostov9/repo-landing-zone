29e3893a087d7dfe55e30307a942181505717de7
iliyan-kostov <iliyan.kostov@email.ikostov.org>
2025-03-03 23:37:05 +0200

    feat: added
    M  .github/workflows/repo-sync.yaml
    M  shells/github_secrets.sh

--- shells/github_secrets.sh
+++ shells/github_secrets.sh
@@ -28,9 +28,7 @@ github_set_secret(){
             local secret=$(yaml_eval "$repo_count_prefix.secret")
 
             if [[ "$secret" == "true" ]]; then
-
                 echo "Found secrets to sync for git $owner_name/$repo_name"
-                # gh secret set "$env_name" --repo "$owner_name/$repo_name" --body "$env_value"
                 gh secret set -f .env --repo "$owner_name/$repo_name"
             fi
         done
